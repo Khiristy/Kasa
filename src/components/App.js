512fd6fae8c3f0c29baf30a8d63e12587fe9556a
Khiristy <147670388+Khiristy@users.noreply.github.com>
2024-03-05 16:49:26 +0100

    updates folders

---
+++ src/components/App.js
@@ -0,0 +1,28 @@
+
+import '../styles/App.css';
+import Banner from './Banner.js';
+
+function App() {
+  return (
+    <Banner />
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //       <h1>Bonjour</h1>
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+  );
+}
+
+export default App;
---
+++ src/components/Banner.js
@@ -0,0 +1,11 @@
+import logo from '../assets/logo.svg';
+
+
+function Banner() {
+    return (
+    
+    <img src={logo} className="App-logo" alt="logo" />)
+    
+}
+
+export default Banner
